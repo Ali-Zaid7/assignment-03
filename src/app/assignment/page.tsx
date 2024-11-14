6ef0c4369847f733597f90dc611cd09abc2da287
Ali-zaid <az7425328@gmail.com>
2024-11-14 08:17:05 -0800

    Your commit message

---
+++ src/app/assignment/page.tsx
@@ -0,0 +1,7 @@
+const Assignment = () => {
+    return (
+      <div className="text-center font-18xl bg-amber-600">I am working on Assignment.When it will be ready InshaAllah I will attached it their and submit it</div>
+    )
+  }
+  
+  export default Assignment;
---
+++ src/app/header/page.tsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return (
+    <div className="bg-blue-600 text-4xl text-white py-6 container mx-auto text-center">This is Header page</div>
+  )
+}
+
+export default Header;
